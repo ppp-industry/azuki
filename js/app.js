520bf57c28295c12516bf1e74079cd956aa3b193
skliarenko <yurasklyarenko33@gmail.com>
2022-10-06 10:55:40 +0300

    navigator

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
+const desktop = document.querySelector('.desktop-mirror');
+const mobileItem = document.querySelector('.mobile-mirror');
+
+const expression = /(iPhone|iPod|iPad|BlackBerry)/i;
+
+console.log(navigator)
+
+if (expression.test(navigator.userAgent)) {
+    desktop.style.display = 'none';
+    mobileItem.style.display = 'block';
+} else {
+    desktop.style.display = 'block';
+    mobileItem.style.display = 'none';
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-
-    const desktop = document.querySelector('.desktop-mirror');
-    const mobileItem = document.querySelector('.mobile-mirror');
-
-    const expression = /(iPhone|iPod|iPad|BlackBerry)/i;
-
-    console.log(navigator.userAgent)
-
-    if (expression.test(navigator.userAgent)) {
-            desktop.style.display = 'none';
-            mobileItem.style.display = 'block';
-    } else {
-            desktop.style.display = 'block';
-            mobileItem.style.display = 'none';
-    }
 
     // const devices = new RegExp('Android|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini', "i");
     // const isApple = ['iPhone', 'iPad', 'iPod', 'iPad Simulator', 'iPhone Simulator', 'iPod Simulator',].includes(navigator.platform)
